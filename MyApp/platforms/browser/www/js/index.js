cc384f19a0885d55c6409de8b63915d84606a239
matriiixu <oani1_16@uni.worc.ac.uk>
2017-11-13 10:32:40 +0000

    Update 1.1

--- MyApp/platforms/browser/www/js/index.js
+++ MyApp/platforms/browser/www/js/index.js
@@ -1,9 +1,11 @@
-
 function random() {
     var val = !Math.round(Math.random());
 
     alert(val);
-    
-    navigator.notification.beep(1); //beep once
-    navigator.notification.beep(2); //beep twice
+    if(val == true){
+       navigator.notification.beep(1); //beep once
+    }
+    if(val == false){
+       navigator.notification.beep(2); //beep twice
+    }
 }
