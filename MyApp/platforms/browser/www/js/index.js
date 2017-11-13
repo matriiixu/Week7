965a366c8aa27702385e992e13cdee26888c192b
matriiixu <oani1_16@uni.worc.ac.uk>
2017-11-13 10:54:25 +0000

    Update

--- MyApp/platforms/browser/www/js/index.js
+++ MyApp/platforms/browser/www/js/index.js
@@ -4,8 +4,7 @@ function random() {
     alert(val);
     if(val == true){
        navigator.notification.beep(1); //beep once
-    }
-    if(val == false){
-       navigator.notification.beep(2); //beep twice
+    }else{
+        navigator.notification.beep(2); //beep twice
     }
 }
