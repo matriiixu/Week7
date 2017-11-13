e94ad28b7343b644bb18e916256bb236ca6540a1
matriiixu <oani1_16@uni.worc.ac.uk>
2017-11-13 11:56:26 +0000

    Update2.1

--- MyApp/platforms/browser/www/js/index.js
+++ MyApp/platforms/browser/www/js/index.js
@@ -9,16 +9,17 @@ function onDeviceReady(){
 function sound() {
     var val = !Math.round(Math.random());
 
-    alert(val);
     if(val == true){
        navigator.notification.beep(1); //beep once
+        navigator.vibrate(2000);
     }else{
         navigator.notification.beep(2); //beep twice
+        navigator.vibrate(200);
     }
 }
 
 function vibrate(){
     //vibrate for 2000 milliseconds
     alert('Pressed!');
-    navigator.vibrate(2000);
+    
 }
